bf6f6fd106c08022646a068d527ec3b26191f3cc
John Barbero Unenge <john.barbero.unenge@gmail.com>
2013-11-09 18:19:16 +0100

    Fix aliases

--- fish/config.fish
+++ fish/config.fish
@@ -6,6 +6,24 @@ set fish_theme gianu
 
 set fish_plugins git
 
+function natti
+    sudo shutdown -h now
+end
+
+function gs
+    git status
+end
+
+function pubkey
+    cat ~/.ssh/id_rsa.pub | xclip -selection clipboard | echo \"Copied public key to clipboard...\"
+end
+
+function dvorak
+    setxkbmap "se(svdvorak)"
+end
+function se
+    setxkbmap se
+end
 # Which plugins would you like to load? (plugins can be found in ~/.oh-my-fish/plugins/*)
 # Custom plugins may be added to ~/.oh-my-fish/custom/plugins/
 # Example format: set fish_plugins autojump bundler
