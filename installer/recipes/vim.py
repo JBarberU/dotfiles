fac55cb4661ad2b9111b4b96bbbc291b53a67f56
John Barbero Unenge <john.barbero.unenge@gmail.com>
2015-01-20 15:12:12 +0100

    Write VimRecipe

--- installer/recipes/vim.py
+++ installer/recipes/vim.py
@@ -1,9 +1,25 @@
 from recipe_base import RecipeBase
+from output_pipe import OutputPipe
+from command import run_cmd_ret_output
+from log import Log
+
 
 class VimRecipe(RecipeBase):
+
+  links = [
+        ("vim/vim", ".vim"),
+        ("vim/vimrc", ".vimrc"),
+      ]
+
   def install(self):
-    print "installing vim"
+    pipe = OutputPipe()
+    if run_cmd_ret_output(["git", "submodule", "update", "--init"], pipe):
+      Log.err("Unable to update git submodules")
+      return
+
+    RecipeBase.install(self)
+
 
   def uninstall(self):
-    print "uninstalling vim"
+    RecipeBase.uninstall(self)
 
