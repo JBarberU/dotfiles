b0ebb170d381451ea789b37d506d1badcb9c6b69
John Barbero Unenge <john.barbero.unenge@gmail.com>
2015-01-20 15:25:09 +0100

    Write IrssiRecipe

--- installer/recipes/irssi.py
+++ installer/recipes/irssi.py
@@ -1,10 +1,17 @@
+from os import path
+from shutil import copyfile
 from recipe_base import RecipeBase
+from log import Log
 
 class IrssiRecipe(RecipeBase):
+
+  links = [("irssi", ".irssi")]
+
   def install(self):
-    print "installing irssi"
+    config = "{0}/irssi/config".format(self.path)
+    if not path.exists(config):
+      copyfile("{0}.example".format(config), config)
+      Log.info("{0} created, tweak it to suit your needs")
 
-  def uninstall(self):
-    print "uninstalling irssi"
-
+    RecipeBase.install(self)
 
