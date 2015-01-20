e56daaf49ba905c1cf7c8dac3982db9841f95fb0
John Barbero Unenge <john.barbero.unenge@gmail.com>
2015-01-20 16:46:39 +0100

    Write ZshRecipe

--- installer/recipes/zsh.py
+++ installer/recipes/zsh.py
@@ -6,6 +6,21 @@ class ZshRecipe(RecipeBase):
 
   def __init__(self, platform, path, home):
     RecipeBase.__init__(self, platform, path, home)
+    self.links = [
+          ("{0}/zsh/zshrc".format(self.path), "{0}/.zshrc".format(self.home)),
+          ("{0}/zsh/sh_functions".format(self.path), "{0}/.sh_functions".format(self.home)),
+          ("{0}/zsh/oh-my-zsh".format(self.path), "{0}/.oh-my-zsh".format(self.home)),
+      ]
+    if self.platform.mac:
+      self.links += [
+            ("{0}/zsh/zshrc_osx".format(self.path), "{0}/.zshrc_osx".format(self.home)),
+        ]
+    self.touch_files = [
+                  "{0}/.aliases".format(self.home),
+                  "{0}/.paths".format(self.home),
+              ]
 
-
+  def install(self):
+    RecipeBase.install(self)
+    Log.warn("Run \"chsh -s /bin/zsh\" to set zsh as our default shell")
 
