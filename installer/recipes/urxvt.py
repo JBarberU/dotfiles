7117ffb2f43869745e5bd1076185e9b81cfb0e75
John Barbero Unenge <john.barbero.unenge@gmail.com>
2015-01-20 15:30:15 +0100

    Write UrxvtRecipe

--- installer/recipes/urxvt.py
+++ installer/recipes/urxvt.py
@@ -1,11 +1,10 @@
 from recipe_base import RecipeBase
 
 class UrxvtRecipe(RecipeBase):
-  def install(self):
-    if self.platform.linux:
-      Log.msg("urxvt install goes here")
 
-  def uninstall(self):
-    if self.platform.linux:
-      Log.msg("urxvt uninstall goes here")
+  def __init__(self, platform, path, home):
+    if platform.linux:
+      self.links = [("xorg/Xresources", ".Xresources")]
+
+    RecipeBase.__init__(self, platform, path, home)
 
