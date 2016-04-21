ab8e81a0db7b90cc01261bfdb6e025b7e7887156
John Barbero Unenge <john.barbero.unenge@gmail.com>
2016-04-21 09:18:58 +0200

    [install.py] Fix vs_code recipe

--- installer/recipes/vs_code.py
+++ installer/recipes/vs_code.py
@@ -3,8 +3,8 @@ from recipe_base import RecipeBase, DPath, HPath
 class VSCodeRecipe(RecipeBase):
 
   name = "VSCode"
+  mkdir_list = [HPath(".config/Code")]
   links = [
         (DPath("vs_code"), HPath(".config/Code/User"))
     ]
-
 
