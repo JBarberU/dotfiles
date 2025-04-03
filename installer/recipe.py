6b067bf230e6c312f6982c384b4b25dcf3759761
John Barbero Unenge <git@lsrkttn.com>
2025-04-03 13:51:46 +0200

    [install] Move installer around

---
+++ installer/recipe.py
@@ -0,0 +1,147 @@
+import os.path as os_path
+
+
+class Target:
+    '''Container for file operations that require a source and destination
+    '''
+
+    def __init__(self, src, dst):
+        self.src = src
+        self.dst = dst
+
+    def __str__(self):
+        return f'{self.src} -> {self.dst}'
+
+
+class Recipe:
+    '''Container for installation process
+    '''
+
+    def __init__(self, dotfile_dir, home_dir, verbose=False):
+        self.dotfile_dir = dotfile_dir
+        self.home_dir = home_dir
+        self.verbose = verbose
+
+        self.binaries = []
+        self.callbacks = []
+        self.copies = []
+        self.custom_commands = []
+        self.files = []
+        self.links = []
+        self.new_dirs = []
+
+    def create_dir(self, path, absolute=False):
+        '''Add instruction to create a directory
+        '''
+        p = path if absolute else os_path.join(self.home_dir, path)
+        self.new_dirs.append(p)
+
+    def create_link(self, src, target, absolute=False):
+        '''Add instruction to create a symbolic link
+        '''
+        src = os_path.join(self.dotfile_dir, src)
+        dst = target if absolute else os_path.join(self.home_dir, target)
+        self.links.append(Target(src, dst))
+
+    def create_copy(self, src, target):
+        '''Add instruction to copy a file
+        '''
+        src = os_path.join(self.dotfile_dir, src)
+        dst = os_path.join(self.home_dir, target)
+        self.copies.append(Target(src, dst))
+
+    def create_file(self, path, absolute=False):
+        '''Add instruction to create an empty file
+        '''
+        p = path if absolute else os_path.join(self.home_dir, path)
+        self.files.append(p)
+
+    def install_binaries(self, binaries):
+        '''Add instruction to install a binary or list of binaries
+        '''
+        if isinstance(binaries, str):
+            self.binaries.append(binaries)
+        elif isinstance(binaries, list):
+            self.binaries = self.binaries + binaries
+        else:
+            raise Exception('Expected list or string')
+
+    def clone(self, repo, path):
+        self.create_custom_commands([
+            'sh', '-c', f'git clone {repo} {path}'
+        ])
+
+    def create_custom_commands(self, command):
+        self.custom_commands.append(command)
+
+    def run_code(self, callback):
+        self.callbacks.append(callback)
+
+    def dryrun(self):
+        '''Print what would be done
+        '''
+        print('Installing binaries:\n'  + ''.join('- ' + str(v) + '\n' for v in self.binaries))
+        print('Copying:\n'              + ''.join('- ' + str(v) + '\n' for v in self.copies))
+        print(f'Running {len(self.callbacks)} callbacks')
+        print('Running commands:\n'              + ''.join('- ' + str(v) + '\n' for v in self.custom_commands))
+        print('Creating files:\n'       + ''.join('- ' + str(v) + '\n' for v in self.files))
+        print('Linking:\n'              + ''.join('- ' + str(v) + '\n' for v in self.links))
+        print('Creating directories:\n' + ''.join('- ' + str(v) + '\n' for v in self.new_dirs))
+
+    def run(self):
+        '''Run installation
+        '''
+
+        if self.binaries:
+            run_command(
+                cmd=['apt-get', 'install'] + self.binaries,
+                needs_elevation=True
+            )
+
+        for target in self.copies:
+            run_command(
+                cmd=['cp', target.src, target.dst],
+                needs_elevation=unable_to_write(target.dst)
+            )
+
+        for file in self.files:
+            run_command(
+                cmd=['touch', file],
+                needs_elevation=unable_to_write(file)
+            )
+
+
+        for link in self.links:
+            dst_path = os.path.dirname(link.dst)
+            if not os.path.exists(dst_path):
+                run_command(
+                        cmd=['mkdir', '-p', dst_path],
+                        needs_elevation=unable_to_write(dst_path)
+                )
+
+            if os.path.islink(link.dst):
+                print(f'{link.dst} already exists, skipping')
+                continue
+
+            run_command(
+                cmd=['ln', '-s', link.src, link.dst],
+                needs_elevation=unable_to_write(link.dst)
+            )
+
+        for directory in self.new_dirs:
+            run_command(
+                cmd=['mkdir', '-p', directory],
+                needs_elevation=unable_to_write(directory)
+            )
+
+        for command in self.custom_commands:
+            run_command(cmd=command, needs_elevation=False)
+
+        for callback in self.callbacks:
+            callback()
+
+    def do_if(self, condition):
+        '''Returns self if condition evaluates to True, or an empty Dummy
+        recipe otherwise
+        '''
+        return self if condition else Recipe('dummy_dotfiles', 'dummy_home')
---
+++ installer/util.py
@@ -0,0 +1,79 @@
+from shutil import which as shutil_which
+from os import (
+    path as os_path,
+    access as os_access,
+    W_OK as os_W_OK,
+)
+from subprocess import Popen as subprocess_Popen
+
+
+def has_binary(binary):
+    '''Checks if the given binary is an existing executable
+    '''
+    return shutil_which(binary) is not None
+
+
+def hasnt_binary(binary):
+    '''Checks if the given binary is not an existing executable
+    '''
+    return not has_binary(binary)
+
+
+def find_existing_base_dir(path):
+    '''Finds the parth of a given path that exists
+    '''
+    if not os_path.isdir(path):
+        return find_existing_base_dir(os_path.dirname(path))
+    if os_path.exists(path):
+        return path
+    return find_existing_base_dir(os_path.dirname(path))
+
+
+def can_write(path):
+    '''Checks if the user can write to the given path
+    '''
+    return os_access(
+        path
+        if os_path.exists(path)
+        else find_existing_base_dir(path),
+        os_W_OK
+    )
+
+
+def unable_to_write(path):
+    '''Negation of can_write
+    '''
+    return not can_write(path)
+
+
+def run_command(cmd, needs_elevation):
+    '''Runs the given command through subprocess
+    '''
+    if VERBOSE:
+        cmd_str = ' '.join(cmd)
+        msg = (
+            'Running command as root' if needs_elevation
+            else 'Running command'
+        )
+        print(f'{msg}: {cmd_str}')
+    with subprocess_Popen(cmd if not needs_elevation else ['sudo'] + cmd):
+        pass
+
+
+def select_option(message, options):
+    '''
+    Takes the given options, sorts them, shows a number and asks the user to
+    pick one of them.
+    '''
+    opts = sorted(options)
+    print(message + ''.join(
+        f'\n{i}: {str(v)}' for i, v in enumerate(opts))
+    )
+    try:
+        ans = input('Selection: ')
+        selected = int(ans)
+        if selected < 0 or selected >= len(opts):
+            raise Exception('Index out of range!')
+        return opts[selected]
+    except ValueError as exc:
+        raise Exception(f'{ans} is not a number') from exc
